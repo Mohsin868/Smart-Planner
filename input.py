df1b75379858253362074467b9abe8c65ff60431
Mohsin868 <mohsin888008@gmail.com>
2026-02-02 15:49:37 +0500

    Add files via upload

--- input.py
+++ input.py
@@ -36,3 +36,51 @@ def delete_task(task_id, user_id):
 
     conn.commit()
     conn.close()
+
+def update_task_status(task_id, user_id, new_status):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute(
+        "UPDATE tasks SET status = ? WHERE id = ? AND user_id = ?",
+        (new_status, task_id, user_id)
+    )
+
+    conn.commit()
+    conn.close()
+
+def delete_all_pending_tasks(user_id):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute(
+        "DELETE FROM tasks WHERE user_id = ? AND status = 'Pending'",
+        (user_id,)
+    )
+
+    conn.commit()
+    conn.close()
+
+def delete_all_completed_tasks(user_id):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute(
+        "DELETE FROM tasks WHERE user_id = ? AND status = 'Completed'",
+        (user_id,)
+    )
+
+    conn.commit()
+    conn.close()
+
+def delete_all_tasks(user_id):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute(
+        "DELETE FROM tasks WHERE user_id = ?",
+        (user_id,)
+    )
+
+    conn.commit()
+    conn.close()
